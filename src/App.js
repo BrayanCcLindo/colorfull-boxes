f0639b078577d41722f1b04e888fd14a8e25783f
Brayan Ccari Lindo <brayancclindo@gmail.com>
2023-09-08 11:37:14 -0500

    Fiddle meta Challenge

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import "./App.css";
 import List from "./List";
 import { colorBox } from "./api/items";
 
+// Implement a feature to allow item selection with the following requirements:
+// 1. Clicking an item selects/unselects it.
+// 2. Multiple items can be selected at a time.
+// 3. Make sure to avoid unnecessary re-renders of each list item in the big list (performance).
+// 4. Currently selected items should be visually highlighted.
+// 5. Currently selected items' names should be shown at the top of the page.
+//
+// Feel free to change the component structure at will.
+
 function App() {
   const items = colorBox;
   return <List items={items} />;
